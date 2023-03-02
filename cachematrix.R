13814186d0bfa7128ff07e6d48725b109c1337f1
edgar-00-00 <edgarmingwei@gmail.com>
2023-03-02 17:02:04 +0800

    Implemented cached optimized matrix inversion function

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,39 @@
-## Put comments here that give an overall description of what your
-## functions do
+## This set of functions uses caching to optimize 
+## calculation of inverse of a matrix
 
-## Write a short comment describing this function
+## makeCacheMatrix - takes argument matrix and 
+## returns list of functions to support optimized 
+## calculation of inverse of matrix
 
 makeCacheMatrix <- function(x = matrix()) {
+	minv <- NULL
+      set <- function(y) {
+      	x <<- y
+            minv <<- NULL
+      }
+      get <- function() x
+	setinverse <- function(m) minv <<- m
+	getinverse <- function() minv
 
+	list(set=set, get=get, 
+		setinverse=setinverse, 
+		getinverse=getinverse)
 }
 
 
-## Write a short comment describing this function
+## cacheSolve - takes argument of list of functions 
+## returned from makeCacheMatrix and 
+## returns the inverse of the matrix
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+      ## Return a matrix that is the inverse of 'x'
+	minv <- x$getinverse()
+	if(!is.null(minv)){
+		message("getting cached data")
+		return(minv)
+	}
+	m <- x$get()
+	minv <- solve(m)
+	x$setinverse(minv)
+	minv
 }
